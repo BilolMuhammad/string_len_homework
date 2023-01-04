06e46ac3a12547346bd5df130bfd556291ab4c02
PRESTIGE <mamatqulov@gmail.com>
2023-01-04 21:52:25 +0500

    07

--- string_len07.py
+++ string_len07.py
@@ -1,6 +1,7 @@
 def main(s1, s2, s3):
     """
-    Given three strings, s1, s2 and s3. return their odd lengths, example "[s1, s2]". If there is no odd length, return "[]".
+    Given three strings, s1, s2 and s3. return their odd lengths, example "[s1, s2]". 
+    If there is no odd length, return "[]".
     Args:
         s1: string
         s2: string
