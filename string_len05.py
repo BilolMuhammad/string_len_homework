f8971c4d3a6bc8d8b7a8d219cd1cdeda82dbc790
PRESTIGE <mamatqulov@gmail.com>
2023-01-04 09:56:15 +0500

    05

--- string_len05.py
+++ string_len05.py
@@ -1,4 +1,4 @@
-def main(s1,s2):
+def main(s1, s2):
     """
     Given two strings, s1 and s2. Find their total length.
     Args:
@@ -7,4 +7,9 @@ def main(s1,s2):
     Returns:
         total length of strings
     """
-    return
+    return len(s1)+len(s2)
+
+
+print(main('sdf', '.,%&@'))
+print(main('as#q', 'aqwed'))
+print(main('a@a&e', 'dfd..s^r'))
