921c895d66c89a3db5c2a301e3c61a8911a6c991
PRESTIGE <mamatqulov@gmail.com>
2023-01-04 21:12:42 +0500

    08

--- string_len08.py
+++ string_len08.py
@@ -8,8 +8,15 @@ def main(s):
     Returns:
         str: answer
     """
+    ans = ''
+
+    jft1 = len(s)//2-1
+    jft2 = len(s)//2
     if len(s) % 2 == 0:
-        return len(s)//2
+        ans += s[jft1]
+        ans += s[jft2]
+
+    return ans
 
 
-print(main('fsdfsfkjgh'))
+print(main('assf'))
