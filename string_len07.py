3a2e7e0f79c0ecce7d727a5627e6d17c06dfae77
PRESTIGE <mamatqulov@gmail.com>
2023-01-04 10:27:53 +0500

    07

--- string_len07.py
+++ string_len07.py
@@ -1,4 +1,4 @@
-def main(s1,s2,s3):
+def main(s1, s2, s3):
     """
     Given three strings, s1, s2 and s3. return their odd lengths, example "[s1, s2]". If there is no odd length, return "[]".
     Args:
@@ -8,4 +8,19 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    sum = ''
+    if len(s1) % 2 != 0:
+        sum += s1
+    if len(s2) % 2 != 0:
+        sum += f", {s2}"
+    if len(s3) % 2 != 0:
+        sum += f", {s3}"
+    if len(sum) == 0:
+        return '[]'
+    else:
+        return f'[{sum}]'
+
+
+print(main('ssd', 'sdf', '^fdss'))
+print(main('ssd', 'sdf3', '^fdss'))
+print(main('ssdx', 'csdf', 'v^fdss'))
